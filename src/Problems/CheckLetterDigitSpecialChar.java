e66d0ef37b43d5244a4aa615387ca97fef72995d
Anshu Singh <anshusingh7738@gmail.com>
2024-01-06 10:00:18 +0530

    add new problem CheckLetterDigitSpecialChar

---
+++ src/Problems/CheckLetterDigitSpecialChar.java
@@ -0,0 +1,28 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class CheckLetterDigitSpecialChar {
+
+    public static void main(String[] args) {
+        // you code goes here
+        System.out.println("Enter any Character: ");
+        Scanner sc = new Scanner(System.in);
+
+        char input = sc.next().charAt(0);
+
+        // if-else
+        if (input >= 65 && input <= 90 || input >= 97 && input <= 122) {
+
+            System.out.println("It's Alphabet");
+
+        } else if (input >= 48 && input <= 57) {
+
+            System.out.println("It's Digit");
+
+        } else {
+            System.out.println("It's Special Character");
+        }
+    }
+
+}
