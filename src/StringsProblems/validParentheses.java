eb6370cf08ef814aab68084a40c47bc1a07a39af
anshu <anshu@merchantech.com>
2024-01-10 11:50:29 +0530

    validParentheses problem added

---
+++ src/StringsProblems/validParentheses.java
@@ -0,0 +1,39 @@
+package StringsProblems;
+
+import java.util.Stack;
+
+public class validParentheses {
+    public static boolean check(String s)
+    {
+        Stack st= new Stack();
+        for(char c:s.toCharArray())
+        {
+            if(c=='('||c=='['||c=='{')
+                st.push(c);
+            else {
+                if(st.isEmpty())
+                    return false;
+                {
+                    char top=(char)st.peek();
+                    if(c==')'&&'('==top||c=='}'&&'{'==top||c==']'&&'['==top)
+                    {
+                        st.pop();
+                    }
+                    else
+                        return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+    public static boolean isValid(String s) {
+        boolean ans=check(s);
+        System.out.println("Answer is: "+ans);
+        return ans;
+    }
+
+    public static void main (String args[] ){
+        System.out.println(isValid("([{}])"));
+    }
+
+}
