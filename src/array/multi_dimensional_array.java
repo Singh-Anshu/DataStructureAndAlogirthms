9317d9ce93d838d84834a9ae8af7a14cd312256e
anshu <anshu@merchantech.com>
2024-01-10 11:37:55 +0530

    add @d Array questions

---
+++ src/array/multi_dimensional_array.java
@@ -0,0 +1,32 @@
+package array;
+
+public class multi_dimensional_array {
+
+    static {
+        System.out.println("The 2D array is:");
+    }
+
+    public static void main(String[] args){
+        int num = 1;
+        int[][] a = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3;  j++) {
+                a[i][j] = num;
+                ++num;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3;  j++) {
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        System.out.print("a[1][2] = "+ a[1][2]);
+
+    }
+}
