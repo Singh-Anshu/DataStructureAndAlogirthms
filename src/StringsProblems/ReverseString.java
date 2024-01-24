6a316e5938b28eeb97517e63e9fbb0c94930160f
Anshu Singh <anshusingh7738@gmail.com>
2024-01-24 20:48:43 +0530

    solving string problems

---
+++ src/StringsProblems/ReverseString.java
@@ -0,0 +1,24 @@
+package StringsProblems;
+
+import java.util.Arrays;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+    }
+
+    public static void reverseString(char[] s) {
+
+        int l = 0;
+        int r = s.length - 1;
+
+        while (l < r) {
+            char temp = s[l];
+            s[l++] = s[r];
+            s[r--] = temp;
+        }
+        System.out.print(s);
+    }
+}
