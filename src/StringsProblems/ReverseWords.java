e6d18f7d1fcf47104caaf5be0c531b1855b3c7b3
anshu <anshu@merchantech.com>
2024-01-25 11:57:28 +0530

    solving strings problems

---
+++ src/StringsProblems/ReverseWords.java
@@ -0,0 +1,20 @@
+package StringsProblems;
+
+public class ReverseWords {
+
+    public static void main(String[] args) {
+
+
+        String[] inputs = "i want to learn android development".split(" ");
+        String result = "";
+
+        for (int i = inputs.length - 1; i >= 0;  i--) {
+
+            result += inputs[i]+" ";
+        }
+
+        //System.out.println(result.trim());
+        System.out.println(result.substring(0, result.length() - 1));
+
+    }
+}
