070b3e1b424c7a0ffd10b0d858460e82275b74e3
Anshu Singh <anshusingh7738@gmail.com>
2024-01-31 23:07:23 +0530

    solving string problems

---
+++ src/StringsProblems/AnagramString.java
@@ -0,0 +1,41 @@
+package StringsProblems;
+
+import java.util.Scanner;
+
+public class AnagramString {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter string one: ");
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        System.out.println("Enter string two: ");
+        String s2 = sc.next();
+
+        System.out.println("Anagram String is : "+ checkStringIsAnagram(s1, s2));
+
+        sc.close();
+    }
+
+    private static boolean checkStringIsAnagram(String s1, String s2) {
+
+        boolean isAnagram = false;
+        //car
+        //act
+
+        for (int i =0 ; i < s1.length(); i++){
+            isAnagram = false;
+            for(int j=0; j < s2.length(); j++){
+
+                if(s1.charAt(i)  == s2.charAt(j)){
+                    isAnagram = true;
+                    break;
+                }
+            }
+            if(!isAnagram){
+                break;
+            }
+        }
+        return isAnagram;
+    }
+}
