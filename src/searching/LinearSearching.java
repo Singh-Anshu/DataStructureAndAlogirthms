b50ee80f67e16b50f724ea5c47893eaea95a731b
anshu <anshu@merchantech.com>
2024-02-08 14:59:27 +0530

    update: working on Linear searching.

---
+++ src/searching/LinearSearching.java
@@ -0,0 +1,14 @@
+package searching;
+
+public class LinearSearching {
+
+    /*
+
+    Linear Search is a very simple search algorithm. In this type of search, a sequential search is made over all items one by one.
+    Every Item is checked and if a match is found then that particular item is returned, otherwise the search continues till the end
+    of the data collection.
+     */
+    public static void main(String[] args) {
+
+    }
+}
