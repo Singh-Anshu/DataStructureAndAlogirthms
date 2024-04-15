b05832bf8f6281170949fdba97e259457de3311a
Anshu Singh <anshusingh7738@gmail.com>
2024-04-15 12:44:36 +0530

    update: temp

---
+++ src/stack/InfixToPostfix.java
@@ -0,0 +1,26 @@
+package stack;
+
+public class InfixToPostfix {
+
+    /*
+    * Problem Statement:
+    * Write a program to convert an Infix expression to Postfix form.
+
+    Infix expression: The expression of the form “a operator b” (a + b) i.e., when an operator is in-between every pair of operands.
+    Postfix expression: The expression of the form “a b operator” (ab+) i.e., When every pair of operands is followed by an operator.
+
+    Examples:
+
+    Input: A + B * C + D
+    Output: ABC*+D+
+
+    Input: ((A + B) – C * (D / E)) + F
+    Output: AB+CDE/*-F+
+
+    */
+
+    public static void main(String[] args) {
+
+
+    }
+}
