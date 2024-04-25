d8183e75db57af25d34085dc52e438e0e7b9a226
Anshu Singh <anshusingh7738@gmail.com>
2024-04-25 12:42:21 +0530

    update: working on queue problems.

---
+++ src/queue/QueueUsingLL.java
@@ -0,0 +1,111 @@
+package queue;
+
+public class QueueUsingLL {
+
+    public static void main(String[] args) {
+
+        QueueLL queueLL = new QueueLL();
+        queueLL.enqueue(10);
+        queueLL.enqueue(20);
+        queueLL.enqueue(30);
+        queueLL.enqueue(40);
+        queueLL.enqueue(50);
+        System.out.println("isEmpty: "+ queueLL.isEmpty());
+        System.out.println("Dequeue: "+ queueLL.dequeue());
+        System.out.println("Peek: "+ queueLL.peek());
+        System.out.println("Size: "+ queueLL.getSize());
+        queueLL.viewQueueLL();
+
+    }
+
+  static   class QueueLL {
+
+        class Node {
+            private int data;
+            private Node next;
+
+            public Node(int data) {
+                this.data = data;
+                next = null;
+            }
+        }
+
+        private Node front;
+        private Node rear;
+        private int size;
+
+        public QueueLL() {
+            this.front = null;
+            this.rear = null;
+            this.size = 0;
+        }
+
+
+        public boolean isEmpty() {
+            return (front == null && rear == null);
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+
+        public void enqueue(int data) {
+            Node n = new Node(data);
+            if (isEmpty()) {
+                front = rear = n;
+            } else {
+                rear.next = n;
+                rear = n;
+            }
+
+            size++;
+        }
+
+        public int dequeue() {
+            if (isEmpty()) {
+                System.out.println("Queue is Empty!");
+                return -404;
+
+            } else if (front == rear) {
+
+                Node node = front;
+                front = rear = null;
+                return node.data;
+
+            } else {
+
+                Node deNode = front;
+                front = front.next;
+                deNode.next = null;
+                size--;
+                return deNode.data;
+
+            }
+
+        }
+
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty!");
+                return -404;
+            }
+
+            return front.data;
+        }
+
+        public void viewQueueLL(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty!");
+                return;
+            }
+
+            // 1->2->3->4->5->
+            Node t = front;
+            while (t != null){
+                System.out.println(t.data+ " ");
+                t = t.next;
+            }
+        }
+    }
+}
